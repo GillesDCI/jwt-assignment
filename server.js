6d9a776bc385c727abc28625cd4fe95d1675d5db
Gilles <gilles.vanhaecke-freelancer@digitalcareerinstitute.org>
2021-07-07 23:41:18 +0200

    changes

--- server.js
+++ server.js
@@ -1,16 +1,7 @@
 const express = require('express');
-const mongoose = require('mongoose');
 require('dotenv').config;
 
 const app = express();
 
-
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`
-,{
-   useNewUrlParser:true, 
-   useUnifiedTopology:true
-})
-.then(() => {console.log("we are connected to the database.")})
-.catch((error) => { console.log('an error occurred while connecting ot the db', error)})
 
 app.listen(3000, () => console.log('The webserver is running on port 3000'));
