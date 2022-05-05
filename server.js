ae7e036bbcc38c053e700952ad70d7ec35cec7be
Franco Speziali <franco.speziali@digitalcareerinstitute.org>
2022-05-05 12:31:33 +0200

    updated packages, converted to esm, updated instructions

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const express = require('express');
-require('dotenv').config;
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-
-app.listen(3000, () => console.log('The webserver is running on port 3000'));
+app.listen(3000, () => console.log("The webserver is running on port 3000"));
